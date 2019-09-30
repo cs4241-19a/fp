56d7df0bc9ca0236832c35d229c52ce02d19f10c
Nkalish <noblekalish1220@gmail.com>
2019-09-30 12:04:42 -0400

    gulp works

--- server.js
+++ server.js
@@ -7,16 +7,18 @@ let port = process.env.PORT || 3000;
 
 const app = express();
 app.use(helmet());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(compression());
 
 app.use(express.static('public'));
 
-app.get('/', function(request, response) {
+app.get('/', function (request, response) {
+    'use strict';
     response.sendFile(__dirname + '/views/index.html');
 });
 
 app.listen(port, function () {
+    'use strict';
     console.log(`Example app listening on port !`);
 });
