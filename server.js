c95ec45559d8842d703a0c04a9d25ecd0dc6cb49
kandonie <kandonie@wpi.edu>
2019-09-30 12:34:15 -0400

    started adding passport

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const compression = require('compression');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 
 let port = process.env.PORT || 3000;
 
@@ -12,10 +14,23 @@ app.use(bodyParser.json());
 app.use(compression());
 
 app.use(express.static('public'));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', function (request, response) {
     'use strict';
     response.sendFile(__dirname + '/views/index.html');
+});
+
+app.post('/login', passport.authenticate('local', { successRedirect: '/',
+    failureRedirect: '/login' }));
+
+app.post('/login',
+    passport.authenticate('local'),
+    function(req, res) {
+        // If this function gets called, authentication was successful.
+        // `req.user` contains the authenticated user.
+        res.redirect('/users/' + req.user.username);
 });
 
 app.listen(port, function () {
