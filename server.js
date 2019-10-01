09e55f5e475b46706908f1a10e9d97748b6c89c0
Brian-Earl <43723279+Brian-Earl@users.noreply.github.com>
2019-10-01 15:40:12 -0400

    Added to server and editted index.html

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ const express = require('express'),
       Rawger = require('rawger'),
       port = 3000
 
+let owned = null;
+async function getAccount(){
+	const rawger = await Rawger();
+	const { users } = rawger;
+	owned = (await users('ThisIsATest').games('owned')).raw();
+	console.log(owned)
+	}
+getAccount()
+
 app.get('/', function (req, res) {
   res.sendFile('/views/index.html', { root: '.' })
 })
@@ -14,6 +23,15 @@ app.get('/games', function (req, res) {
 })
 app.get('/catalog', function (req, res) {
   res.sendFile('/views/catalog.html', { root: '.' })
+})
+
+app.get('/getgames', function (req, res) {
+  res.send(owned);
+})
+
+app.get('/gamedata',  function(req, res) {
+  console.log("You Got The Game: " + res.game)
+	res.send()
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
