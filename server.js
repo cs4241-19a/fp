ec8fffc875003e82fab0381ab473253f8ca2cab9
jefff <ryan@autoreg-119616.dyn.wpi.edu>
2019-10-01 15:17:28 -0400

    added to server

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ const port = 3000
 app.get('/', function (req, res) {
   res.sendFile('/views/index.html', { root: '.' })
 })
+app.get('/requests', function (req, res) {
+  res.sendFile('/views/requests.html', { root: '.' })
+})
+app.get('/games', function (req, res) {
+  res.sendFile('/views/games.html', { root: '.' })
+})
+app.get('/catalog', function (req, res) {
+  res.sendFile('/views/catalog.html', { root: '.' })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
