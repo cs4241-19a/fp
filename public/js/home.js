5d82336a030fa8451395d1d5a0e468a9973f3384
Elie <eliemhess@gmail.com>
2019-10-02 15:14:13 -0400

    Minor changes
    Delete dropdown, clean up login page, fix typos, code style

---
+++ public/js/home.js
@@ -0,0 +1,5 @@
+let currentUsername = "Current User"
+
+//TODO: Need to set currentUsername to be the, well, current username
+
+document.getElementById("logoutLink").innerHTML = "Log out " + currentUsername
