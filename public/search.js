1e180e29ddc261875cae0adfb965ee20939a0d94
Caitlin Enright <cenright@wpi.edu>
2019-10-03 13:24:05 -0400

    Added bootsrap table with sample data

---
+++ public/search.js
@@ -0,0 +1,27 @@
+console.log("we got to script")
+//let json = './tracks.js'
+//const json = require('tracks.js');
+
+window.onload = function(e){
+    let searchButton = document.getElementById("songSearch");
+    searchButton.onclick = search;
+    
+
+}
+
+function search() {
+    let songName = document.getElementById("songName").value
+
+    let listOfSongNames = []
+    let tracks = tracksJson.tracks.items;
+
+    console.log(tracksJson);
+
+    console.log("Number of tracks is " + tracks.length);
+
+    for(let i = 0; i < tracks.length; i++) {
+        console.log(tracks[i].name);
+    }
+
+
+}
