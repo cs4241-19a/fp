57725c0a993f24a92a2f66cfaf5c985918a01e85
Evan Goldstein <evan@icarusnet.me>
2019-10-05 17:35:18 -0400

    MaxMind DB, promises

--- database.js
+++ database.js
@@ -1,26 +1,50 @@
+const Maxmind = require('@maxmind/geoip2-node').Reader;
+
 const MongoClient = require('mongodb').MongoClient;
 const url = "mongodb+srv://" + process.env.DB_USER + ":" + process.env.DB_PASS + "@faviconmap-j8xwi.mongodb.net/FaviconMap?retryWrites=true&w=majority&authSource=admin";
-module.exports = function (callback) {
+module.exports = function () {
     const client = new MongoClient(url, {useNewUrlParser: true, useUnifiedTopology: true});
+    // let PingsCollection;
     client.connect().then(() => {
-        client.db('FaviconMap').createCollection('pings')
+        client.db('FaviconMap').createCollection('pings');
     });
+
+    // .then(() => PingsCollection = client.db('FaviconMap').collection('pings'));
+    function PingsCollection() {
+        return new Promise((resolve) => {
+            if (client.isConnected()) resolve(client.db('FaviconMap').collection('pings'));
+            else client.connect().then(() => resolve(client.db('FaviconMap').collection('pings')))
+
+        })
+    }
+
+    function getCity(data, cb) {
+        return new Promise((resolve, reject) => {
+            Maxmind.open('GeoLite2-City.mmdb').then(reader => {
+                for (let i = 0; i < data.length; i++) {
+                    let city = reader.city(data[i].ip);
+                    data[i].lat = city.location.latitude;
+                    data[i].lng = city.location.longitude;
+                    data[i].city = city.city.names.en;
+                }
+                resolve(data);
+            }).catch(error => {
+                reject("Error getting GeoIP info: " + error)
+            });
+        });
+    }
 
     return {
         // Data: [{favicon: "", rtt: 0, ip: ""}]
         insertPings: function (data) {
-            client.connect(function (err, db) {
-                db.db('FaviconMap').collection('pings').insertMany(data).then(() => {
-                    callback();
-                });
+            return new Promise((resolve) => {
+                getCity(data).then(data => PingsCollection().then(col => resolve(col.insertMany(data))));
             });
         },
         // returns [{favicon: "facebook.com", avg_rtt: 1.1, city: "Boston", lat: "0.0", lng: "0.0"}]
-        getData: function (cb) {
-            client.connect(function (err, db) {
-                db.db('FaviconMap').collection('pings').find().toArray(function (err, result) {
-                    cb(err, result);
-                });
+        getData: function () {
+            return new Promise((resolve, reject) => {
+                PingsCollection().then(col => resolve(col.find().toArray())).catch(error => reject(error))
             });
         }
     };
--- dbtester.js
+++ dbtester.js
@@ -1,21 +1,10 @@
 const DB = require('./database');
 
-function testcb() {
-    console.log("insert successful.")
-}
-
-const db = new DB(testcb);
+const db = new DB();
 const testdata = [
-    {favicon: "facebook.com", rtt: 1.2, ip: "31.13.71.36"},
-    {favicon: "google.com", rtt: 2.1, ip: "172.217.9.238"},
-    {favicon: "reddit.com", rtt: 0.1, ip: "151.101.193.140"}
+    {favicon: "facebook.com", rtt: 1.2, ip: "72.93.244.26"},
 ];
-db.insertPings(testdata);
+db.insertPings(testdata).then(result => console.log(result));
 
-
-//example of calling getData
-db.getData(function (err, data) {
-    if (err) throw err;
-    console.log(data)
-});
+db.getData().then(data => console.log(data));
 
