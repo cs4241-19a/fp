6bd5724c18a883f05e232b8f3cbf3eb989672199
Pedro de Vasconcellos Oporto <pdevasconcelloso@wpi.edu>
2019-10-06 18:13:47 -0400

    Added update and get functions to firestore_service

--- firestore_service.js
+++ firestore_service.js
@@ -9,6 +9,46 @@ exports.init = function(){
     this.firestore = admin.firestore()
 }
 
-exports.getRandomData = function (){
-    return {}
+exports.addNewProfile = function (new_profile){
+    let profiles_coll = this.firestore.collection("music_profiles")
+    profiles_coll.doc(new_profile.User.Username).set(new_profile)
+}
+
+exports.addMusicToProfile = function (username, song){
+    let profiles = this.firestore.collection("music_profiles")
+    profiles.doc(username).get().then(function(obj){
+        prof = obj.data()
+        prof.songs.push(song)
+        profiles.doc(username).update(prof)
+    })
+    let posts = this.firestore.collection("posts")
+    posts.add({
+        user: username,
+        date: song.uploaded,
+        itemid: song.itemid
+    })
+}
+
+exports.getMostRecentPosts = function (limit){
+    let profiles = this.firestore.collection("music_profiles")
+    let posts = this.firestore.collection("posts")
+    return new Promise((resolve, reject) => {
+        posts.orderBy("date").limit(limit).get().then(function(list){
+            post_list = []
+            list.data().forEach(p => {
+                profiles.doc(p.user).get().then(function(obj){
+                    prof = obj.data();
+                    song = prof.songs.filter(function(song){
+                        return p.itemid == song.itemid;
+                    })[0];
+                    post_list.push({
+                        user: prof.user,
+                        song: song
+                    }); 
+                })
+            })
+            resolve(post_lists)
+        })
+    })
+    
 }
