fef2170f6cdf9018b7d008508a9e325e148b1949
Elie <eliemhess@gmail.com>
2019-10-06 12:25:07 -0400

    Add refresh button and error checking if there are no recommendations in the db

--- public/js/home.js
+++ public/js/home.js
@@ -1,4 +1,6 @@
 const MAXIMUM_ENTRIES_LOADED = 25
+const refreshBtn = document.getElementById("refresh-btn")
+const list = document.getElementById("recommendation-list")
 
 const getData = function () {
     (async () => {
@@ -16,40 +18,50 @@ const getRecommendations = function () {
             method: 'GET'
         })
         let res = await rawResponse.json()
-        let list = document.getElementById("recommendation-list")
 
-        let maxEntries = res.length
-        if (maxEntries > MAXIMUM_ENTRIES_LOADED) maxEntries = MAXIMUM_ENTRIES_LOADED
+        let numEntries = res.length
+        if (numEntries > MAXIMUM_ENTRIES_LOADED) numEntries = MAXIMUM_ENTRIES_LOADED
 
-        for (let i = 0; i < maxEntries; i++) {
-            let element = res[i]
-            let songname = element.tracknumber //TODO: Replace with code getting song name from Spotify
-            let username = element.username
-            let rating = element.rating
-            let caption = element.caption
-
-            if (isNaN(rating)) {
-                console.log(rating + " - Rating not a number")
-                rating = Math.floor((Math.random() * 5) + 1)
-            } else if (rating < 1) {
-                console.log(rating + " - Rating too low")
-                rating = 1
-            } else if (rating > 5) {
-                console.log(rating + " - Rating too high")
-                rating = 5
-            }
-
-            list.innerHTML +=
-                `<div class="card" id="recommendation${i}">\n` +
+        if (numEntries === 0) {
+            list.innerHTML =
+                `<div class="card" id="rec-error-msg">\n` +
                 `   <div class="card-body">\n` +
-                `      <h5 class="card-title">` + songname + `</h5>\n` +
-                `      <p class="card-text">` + username + " rated this song " + rating + " out of 5" + `</p>\n` +
-                `      <p class="card-text"><i>` + "\"" + caption + "\"" + `</i></p>\n` +
+                `      <p class="card-text">No recommendations found</p>\n` +
                 `   </div>\n` +
-                `</div>`;
+                `</div>`
+        } else {
+            list.innerHTML = ""
+            for (let i = 0; i < numEntries; i++) {
+                let element = res[i]
+                let songname = element.tracknumber //TODO: Replace with code getting song name from Spotify
+                let username = element.username
+                let rating = element.rating
+                let caption = element.caption
+
+                if (isNaN(rating)) {
+                    console.log(rating + " - Rating not a number")
+                    rating = Math.floor((Math.random() * 5) + 1)
+                } else if (rating < 1) {
+                    console.log(rating + " - Rating too low")
+                    rating = 1
+                } else if (rating > 5) {
+                    console.log(rating + " - Rating too high")
+                    rating = 5
+                }
+
+                list.innerHTML +=
+                    `<div class="card" id="recommendation${i}">\n` +
+                    `   <div class="card-body">\n` +
+                    `      <h5 class="card-title">` + songname + `</h5>\n` +
+                    `      <p class="card-text">` + username + " rated this song " + rating + " out of 5" + `</p>\n` +
+                    `      <p class="card-text"><i>` + "\"" + caption + "\"" + `</i></p>\n` +
+                    `   </div>\n` +
+                    `</div>`
+            }
         }
     })()
 }
 
 getData()
 getRecommendations()
+refreshBtn.onclick = getRecommendations
