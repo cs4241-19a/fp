98047c2c505e214003cbb09618f4d791a3ffef6f
Julian Pinzer <julian.pinzer@gmail.com>
2019-10-06 15:17:59 -0400

    Made MYSQL work, gets a list of users
    Currently using an express server and react server with proxying, trying to figure out if there's a way around it.

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "fp"
+});
+
+con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+
+app.get('/api/hello', (req, res) => {
+    con.query("SELECT * FROM accounts;", function (err, data) {
+        (err)?res.send(err):res.json({users: data});
+    })
+});
+
+app.post('/api/world', (req, res) => {
+    console.log(req.body);
+    res.send(
+        `I received your POST request. This is what you sent me: ${req.body.post}`,
+    );
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
