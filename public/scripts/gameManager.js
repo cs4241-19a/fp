4940ea5bbefed456eb7abd379f8728a0086c6185
Kdoje <comppower15@gmail.com>
2019-10-07 03:29:53 -0400

    Adds character buying and state change

--- public/scripts/gameManager.js
+++ public/scripts/gameManager.js
@@ -6,8 +6,8 @@ class GameManager {
   characterNames = ['blue', 'orange', 'purple', 'red', 'yellow'];
   playerOnePieces;
   playerTwoPieces;
-  buyGroup;
-  state;
+  buyList;
+  state; stateTransitionKey;
   //resources are used as health and currency, once they hit 0 the player loses
   playerOneResources;
   playerOneResourceText;
@@ -21,23 +21,60 @@ class GameManager {
   constructor() {
     this.playerOnePieces = [];
     this.playerTwoPieces = [];
-    this.buyGroup = [];
+    this.buyList = [];
     this.state = CONSTANTS.STATES.buy;
     this.playerOneResources = this.playerTwoResources = CONSTANTS.INIT_RESOURCES;
     this.turn = CONSTANTS.TURN.p1;
   }
 
-  populateBuyList(){
+  /**
+   * destroys all objects in the buylist, then repopulates them and
+   * adds listeners that add them to the desired picelist when clicked
+   * @param pieceList the phaser group to put the pieces in when bought
+   * @param scene the scene to add the sprites to.
+   */
+  initBuyList(pieceList, scene) {
+    //get rid off all children in the build list (just in case)
+    this.clearBuyList();
+    //put all of the characters into the list
+    this._populateBuyList(scene);
+    // the game starts on player1's buy turn so we will have it add the child to that player's pieces
+    // then remove the object from the scene
+    this.buyList.getChildren().forEach((child)=>{
+      child.setInteractive().on('pointerup', function(pointer, localX, localY, event){
+        pieceList.add(child);
+        console.log('added child!!');
+        child.destroy();
+      })
+    })
+  }
+
+  clearBuyList(){
+    this.buyList.getChildren().forEach((child)=>{
+      child.destroy();
+      console.log('removing character');
+    });
+    this.buyList.clear(true);
+  }
+
+  _populateBuyList(scene){
     // pick 3 random characters and center them across the screen for the player to click and purchase
     let x = CONSTANTS.WIDTH/4;
+    let that = this;
     for (let i = 0; i < 3; i++) {
       //how do we inject the state object into this?
-      this.buyGroup.create(x, CONSTANTS.HEIGHT/2, this.characterNames[getRandomInt(this.characterNames.length)]);
+      let character = scene.physics.add.sprite(x,
+          CONSTANTS.HEIGHT/2, this.characterNames[getRandomInt(this.characterNames.length)]);
+      // set a health and strength for the character
+      character.setData({ health: 100, strength: 12});
+      this.buyList.add(character);
       x += 100; //this is an arbitrary length based off the size of the character
     }
   }
 
-  addCharacterToPlayer(player, character)
+  addCharacterToPlayer(player, character) {
+
+  }
 
   /**
    * creates the instance of the GameManager
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -57,7 +57,7 @@ function create() {
   const layer = map.createStaticLayer(0, tiles, 0, CONSTANTS.SETUP_AREA_HEIGHT);
 
   //initialize the groups for player1, player2 and buy area.
-  gameManager.buyGroup = this.physics.add.group();
+  gameManager.buyList = this.physics.add.group();
   gameManager.playerOnePieces = this.physics.add.group();
   gameManager.playerTwoPieces = this.physics.add.group();
   // create the initial text resource counters in the corners
@@ -67,9 +67,11 @@ function create() {
   gameManager.playerTwoResourceText = this.add.text(16, CONSTANTS.HEIGHT-32-16,
       `resources: ${gameManager.playerOneResources}`, { fontSize: '32px', fill: '#fff' });
 
-  // the game starts at buy for player 1, so populate the list
-  gameManager.populateBuyList();
+  //set up the space key for transitions, then initialize the buy list for the first part of the game
+  gameManager.stateTransitionKey = this.input.keyboard.addKey('SPACE');  // Get key object
+  gameManager.initBuyList(gameManager.playerOnePieces, this);
 }
+
 
 /**
  * This is run each frame
@@ -77,7 +79,14 @@ function create() {
 function update() {
   if (gameManager.state === CONSTANTS.STATES.buy) {
     if (gameManager.turn === CONSTANTS.TURN.p1) {
-
+      // if the user presses the space key, change who's buying
+        if(this.input.keyboard.checkDown(gameManager.stateTransitionKey, 50)){
+          gameManager.initBuyList(gameManager.playerTwoPieces, this);
+          gameManager.turn = CONSTANTS.TURN.p2;
+          console.log('changing turns');
+        }
+    }
+    if(gameManager.turn === CONSTANTS.TURN.p2){
 
     }
   }
