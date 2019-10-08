7e8e6aab511de68fd55ea5c406fb3e793f0521e9
Glitch (mersair-fp) <none>
2019-10-08 16:22:20 +0000

    📺💡 Checkpoint ./public/javascripts/login.js:6267243/1917 ./public/pages/home.html:6267243/29

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,40 +1,21 @@
+function logIn() {
+  let username = document.querySelector("#username");
+  let password = document.querySelector("#password");
+  let json = { username: username.value, password: password.value };
+  let body = JSON.stringify(json);
 
+  fetch("/login", {
+    headers: {
+      "Content-Type": "application/json"
+    },
+    method: "POST",
+    body: body
+  }).then(function(res){
+    //parse response?
+  })
+}
 
-function logIn(){
-    location.href='/index'
-    let username = document.querySelector( '#username' )
-    let password = document.querySelector( '#password' )
-    let json = { username: username.value, password: password.value}
-    let URL = "username=" +username.value +  "&password=" + password.value
-  
-    let body = JSON.stringify(json)
-    //let body = URL
-    
-    fetch( '/login', {
-        headers: {
-              'Content-Type': 'application/json',
-              // 'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        method:'POST',
-        body: body
-      }).then(function(response){
-         return response.text();
-        }).then(function (r){
-          if (r == "OK"){
-            location.href='/index.html'
-          }
-          else {
-            alert(r)
-           // alert("Account credentials invalid")
-           //alert("Account credentials invalid")
-           //document.getElementById("baddy").style.display = "inline"
-          }
-    })
-  }
-  
-  
-  window.onload = function() {
-      const button = document.getElementById( 'loginButton' )
-      button.onclick = logIn()
-    }
-  
+window.onload = function() {
+  const button = document.getElementById("loginButton");
+  button.onclick = logIn();
+};
