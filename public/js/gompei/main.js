fbe172ca5c549154b5c185af77644d15ec3e4cff
Andrew <andrewmarkoski@gmail.com>
2019-10-09 01:58:24 -0400

    Added moving ground, help message, and start text

--- public/js/gompei/main.js
+++ public/js/gompei/main.js
@@ -16,51 +16,72 @@ var config = {
         update: update
     }
 };
+
+
 const PLAY_STATE = 0;
 const LOST_STATE = 1;
 const PAUSE_STATE = 2;
+const START_STATE = 3;
+const path = "/assets/gompei/";
+
+let state = START_STATE; // current state
+let newState = false;
 
 let game = new Phaser.Game(config);
 let player;
+let rock = [];
+let grass;
+
 let cursors;
 let pauseKey;
+let spaceBar;
 let groundGroup;
-let rock = [];
-const path = "/assets/gompei/";
+
+
 let score = 0;
 let maxRocks = 3;
 let rockSpeed = -200;
-let state = PLAY_STATE; // current state
+let rockAcc = 0.05;
+
 let scoreText;
+let pauseText;
+let startText;
+
 let playerYVelocity;
-let newState = false;
-let rockAcc = 0.1;
 let extraJumps = 1;
 let jumpsLeft = extraJumps;
 let canJumpAgain = true;
-let pauseText;
+
+
 
 // preload images
 function preload () {
-    this.load.image("gompei", path + "gompei.png");
-    this.load.image("ground", path + "groundLong.png");
-    this.load.image("rock", path + "rock.png");
+    this.load.image("gompei", path + "gompei.png"); // Source: https://depositphotos.com/273483776/stock-illustration-pixel-art-goat-character-isolated.html
+    this.load.image("ground", path + "groundLong.png"); 
+    this.load.image("rock", path + "rock.png"); // Source: http://pixelartmaker.com/art/da268f06e621b21 
+    this.load.image("grass", path + "grassLong.png"); // Source: https://imgbin.com/png/XmJBdYii/pixel-art-game-sprite-png 
 }
 
-// set up images on page with correct properties
+// Set up images on page with correct properties
 function create () {
 
-    // set ground properties
+    // this.cameras.main.backgroundColor.setTo(19, 64, 84);
+
+    // Set ground properties (separate from grass since grass has no physics)
     groundGroup = this.physics.add.staticGroup();
     groundGroup.create(0, 560, "ground");
-    let offset = 700;
+
+    // Set moving ground properties
+    grass = this.add.tileSprite(0,560, this.width, this.height, 'grass')
 
     // set player properties
     player = this.physics.add.sprite(50, 450, "gompei").setScale(0.7);
     this.physics.add.collider(player, groundGroup);
     player.setCollideWorldBounds(true);
+    playerYVelocity = 0;
 
     // set rock properties
+    let offset = 700;
     for (let i = 0; i < maxRocks; i++) {
         rock[i] = this.physics.add.image(offset, 450, "rock").setScale(0.25);
         offset += 450;
@@ -70,18 +91,29 @@ function create () {
         rock[i].body.allowGravity = false;
     }
 
+    // Set up inputs
     cursors = this.input.keyboard.createCursorKeys();
     pauseKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.P);
+    spaceBar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
 
     // Add score HUD
     scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#fff' });
 
     // Set up pause message
-    pauseText = this.add.text(this.cameras.main.centerX, this.cameras.main.centerY, "PAUSED", { fontSize: '32px', fill: '#fff' });
+    pauseText = this.add.text(this.cameras.main.centerX, this.cameras.main.centerY, "PAUSED (\'P\' to resume)", { fontSize: '28px', fill: '#fff' });
     pauseText.setX(this.cameras.main.centerX - pauseText.width / 2);
     pauseText.setY(this.cameras.main.centerY - pauseText.height / 2);
-    pauseText.visible = false;
+    pauseText.visible = false; // Hide pause text
 
+    // Set up start message
+    startText = this.add.text(this.cameras.main.centerX, this.cameras.main.centerY, "Press SPACE to Start!", { fontSize: '28px', fill: '#fff' });
+    startText.setX(this.cameras.main.centerX - pauseText.width / 2);
+    startText.setY(this.cameras.main.centerY - pauseText.height / 2);
+    
+    // Stop everything at start
+    stopPlayer();
+    stopRocks();
+    
 }
 
 // Updates and re draws images
@@ -90,12 +122,10 @@ function update () {
     pauseKey.on('down', function(event){
         if (!newState) {
             if (state === PLAY_STATE) {
-                state = PAUSE_STATE;
                 pauseGame();
                 newState = true;
             }
             else if (state === PAUSE_STATE) {
-                state = PLAY_STATE;
                 resumeGame();
                 newState = true;
             }
@@ -117,29 +147,34 @@ function update () {
         score++;
         scoreText.setText("Score: " + score);
 
+        // Set speed of ground
+        grass.tilePositionX -= rockSpeed / 60;
+
         // Steadily increase rock speed
         rockSpeed-=rockAcc;
         setRockSpeeds(rockSpeed);
 
+        // save player velocity in case of pause
         playerYVelocity = player.body.velocity.y;
     
+        // Allow player to jump again when letting go of up key
         if (!cursors.up.isDown) {
             canJumpAgain = true;
         }
 
-        // jump
+        // Jump when on ground
         if (cursors.up.isDown && player.body.touching.down) {
             player.setVelocityY(-400);
             canJumpAgain = false;
         }
     
-        // fast fall
+        // Fast fall in midair
         else if (cursors.down.isDown && !player.body.touching.down) {   
             player.setVelocityY(player.body.velocity.y + 30);
             
         }
 
-        // double check
+        // Double Jump
         else if (cursors.up.isDown && !player.body.touching.down && jumpsLeft > 0 && canJumpAgain) {
             let newSpeed = -400;
             player.setVelocityY(newSpeed);
@@ -147,8 +182,7 @@ function update () {
             canJumpAgain = false;
         }
 
-
-        // refresh double jump
+        // Refresh double jump when landing on ground
         else if (player.body.touching.down) {
             jumpsLeft = extraJumps;
         }
@@ -156,17 +190,22 @@ function update () {
         // Reset rocks after going off left side of screen
         for (let i = 0; i < maxRocks; i++) {
             if (rock[i].x < 0) {
-                rock[i].x = (Math.random() * 500) + 800
+                rock[i].x = (Math.random() * 600) + 800
             }
         }
     }
+    else if (state === START_STATE){
+        if (spaceBar.isDown) {
+            resumeGame();
+        }
+    }
 }
+
 
 function setRockSpeeds (rockSpeed) {
     for (let i = 0; i < maxRocks; i++) {
         rock[i].setVelocityX(rockSpeed);
     }
-
 }
 
 // Put game in lost state when player collides with a rock
@@ -190,6 +229,7 @@ function rockCollision () {
 }
 
 function pauseGame () {
+    state = PAUSE_STATE;
     stopRocks();
     stopPlayer();
     pauseText.visible = true;
@@ -211,9 +251,11 @@ function stopPlayer () {
 }
 
 function resumeGame () {
+    state = PLAY_STATE;
     resumePlayer();
     resumeRocks();
     pauseText.visible = false;
+    startText.visible = false;
 
 }
 
