87ad21761193625f5d45a2045cd8254147845964
Pedro de Vasconcellos Oporto <pdevasconcelloso@wpi.edu>
2019-10-09 15:53:26 -0400

    added call to get 10 most recent posts

--- src/js/FeedContainer.js
+++ src/js/FeedContainer.js
@@ -6,25 +6,37 @@ import Grid from '@material-ui/core/Grid';
 export default function FeedContainer(props) {
 
     //get this from server somewhere else, maybe store?
-    const items = [
+    var items = [
         {
-            song: {title: "title1", artist: "artist1", file: 0},
+            song: {title: "title1", artist: "artist1", song_id: 0},
             user: {username: "uname1", name: "Bob"},
             options: {height: 0, length: 0},
+            datetime: "20/02/2010"
         },
         {
-            song: {title: "asdf", artist: "asdf", file: 0},
+            song: {title: "asdf", artist: "asdf", song_id: 0},
             user: {username: "-0q9[2 34", name: "Boq23sdfgb"},
             options: {height: 0, length: 0},
+            datetime: "01/01/2012"
         },
         {
-            song: {title: "title", artist: "artist", file: 0},
+            song: {title: "title", artist: "artist", song_id: 0},
             user: {username: "XxX_420_newbRekr_69_XxX", name: "Bob"},
             options: {height: 0, length: 0},
+            datetime: "01/02/2015"
         },
     ]
 
-        //when getting array of 10 items from server
+    fetch('/feed')
+        .then( function( response ) {
+            return response.json();
+        }).then(function (response){
+            response.forEach(feed_item => {
+                items.push(feed_item)
+            });
+        });
+
+    //when getting array of 10 items from server
     //items.map(item) => <FeedItem etcetcetc />
     //will render all feed items automatically from the data
 
