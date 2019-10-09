2ec14ef64212ff577b2036e7007b5531c943e314
elizabethkirschner <eekirschner@wpi.edu>
2019-10-09 11:29:32 -0400

    some initial perlenspiel stuff
    Signed-off-by: elizabethkirschner <eekirschner@wpi.edu>

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var logger = require('morgan'),
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
+var gameRouter = require('./routes/game');
 
 passport.use(new Strategy(
   function(username, password, cb) {
@@ -53,6 +54,7 @@ app.use(express.static('public')); // 3
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/login',loginRouter);
+app.use('/game',gameRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
