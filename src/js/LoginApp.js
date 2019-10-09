0bc8217b5d160482f6ed306c07eb3e9a55449bce
manasmehta18 <manasmehta.18@gmail.com>
2019-10-08 22:49:04 -0400

    added toasts

--- src/js/LoginApp.js
+++ src/js/LoginApp.js
@@ -105,9 +105,8 @@ export default function LoginApp(props) {
 											First Name
 										</label>
 										<input
-											className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+											className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
 											id="first-name" type="text" placeholder="Jane"/>
-										<p className="text-red-500 text-xs italic">Please fill out this field.</p>
 									</div>
 									<div className="w-full md:w-1/2 px-3">
 										<label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
