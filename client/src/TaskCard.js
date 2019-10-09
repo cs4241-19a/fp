e7cc22d17055350a9663ae24eaf77758fa76b25f
Thomas White <twhite@wpi.edu>
2019-10-08 23:56:29 -0400

    Card Wireframe
    First crack at a tech wireframe for the 'simple' view.
    
    Uses the z-index, with absolute positioning, to stack elements.

---
+++ client/src/TaskCard.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Card from "react-bootstrap/Card";
+import Button from 'react-bootstrap/Button';
+
+import './TaskCard.css';
+
+class TaskCard extends React.Component {
+  render() {
+    return (
+    <div className={'card-z-'+this.props.card_z+" w-100 row justify-content-center align-items-center"}>
+      <Card style={{ width: '40rem' }}>
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+          Some quick example text to build on the card title and make up the bulk of
+          the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+    </div>
+    )
+  }
+}
+
+export default TaskCard
