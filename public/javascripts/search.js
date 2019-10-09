602084266aabf3bd098578234507c7a3a70f29d5
Glitch (mersair-fp) <none>
2019-10-09 22:13:02 +0000

    🎢🎈 Checkpoint ./public/javascripts/search.js:6267243/2899

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -1,44 +1,42 @@
+function query(str) {
+  let body = { name: str, crn: null };
+  let json = JSON.stringify(body);
 
-
-function query(str){
-  let body = {name: str};
-  body = JSON.stringify(body)
-
-  fetch("api/books/searchBook", {
+  fetch("api/books/getBooks", {
     headers: {
       "Content-Type": "application/json"
     },
     method: "POST",
-    body: body
+    body: json
   }).then(function(res) {
     res.json().then(function(ret) {
       for (let i = 0; i < ret.length; i++) {
-        alert(ret)
-        let tdNode = document.createElement("td");
-        let tdNode2 = document.createElement("td");
-        let tdNode3 = document.createElement("td");
-        let trNode = document.createElement("tr");
-        tdNode.appendChild(document.createTextNode(ret[i].name));
-        tdNode2.appendChild(document.createTextNode(ret[i].crn));
-        tdNode3.appendChild(document.createTextNode("Physical"));
-        trNode.appendChild(tdNode);
-        trNode.appendChild(tdNode2);
-        trNode.appendChild(tdNode3);
-        document.getElementById("results").appendChild(trNode);
+        alert(ret[i])
+        if (ret[i].name.contains(str)) {
+          let tdNode = document.createElement("td");
+          let tdNode2 = document.createElement("td");
+          let tdNode3 = document.createElement("td");
+          let trNode = document.createElement("tr");
+          tdNode.appendChild(document.createTextNode(ret[i].name));
+          tdNode2.appendChild(document.createTextNode(ret[i].crn));
+          tdNode3.appendChild(document.createTextNode("Physical"));
+          trNode.appendChild(tdNode);
+          trNode.appendChild(tdNode2);
+          trNode.appendChild(tdNode3);
+          document.getElementById("results").appendChild(trNode);
+        }
       }
     });
   });
 }
 
-
 window.onload = function() {
-  document.addEventListener('keypress', function (e) {
+  document.addEventListener("keypress", function(e) {
     var key = e.which || e.keyCode;
-    if (key === 13) { 
+    if (key === 13) {
       let str = document.querySelector("#search");
-       query(str)
-      window.reload()
+      query(str);
+      //window.reload()
     }
-});
+  });
 };
-
