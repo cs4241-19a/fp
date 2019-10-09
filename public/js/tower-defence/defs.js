6cd34fa4f719f99dca26266adab7026541f8ea66
jimikajon <jimi.kajon@gmail.com>
2019-10-08 21:39:13 -0400

    added Tower

--- public/js/tower-defence/defs.js
+++ public/js/tower-defence/defs.js
@@ -10,7 +10,7 @@ const myGame = {
 };
 
 const cellTypes = {
-    BLOCK: -2,  // this block cannot be passed or have anything on it
+    WALL: -2,  // this tile cannot be passed, have anything on it or be shot though
     BASE: -1,
     OPEN: 0,
     TOWER: 1,
@@ -30,27 +30,27 @@ const grid = [];
         grid[i] = [];
         for (let j = 0; j < playArea.width / cellSize.width; j++) {
             if (i === 0 || j === 0 || i === (playArea.height / cellSize.height) - 1 || (i !== enemyEnterCoord.y && j === (playArea.width / cellSize.width) - 1)) {
-                // add BLOCKs to top, bottom, left, and right. Except the second one down on the right
-                grid[i][j] = cellTypes.BLOCK;
+                // add WALLs to top, bottom, left, and right. Except the second one down on the right
+                grid[i][j] = cellTypes.WALL;
             } else {
                 grid[i][j] = cellTypes.OPEN;
             }
         }
     }
     // game and cell size must allow base to fit in 2x2 cells in bottom left corner
-    // overrides some BLOCKs
+    // overrides some WALLs
     grid[btmLeft.y][btmLeft.x] = cellTypes.BASE;
     grid[btmLeft.y][btmLeft.x + 1] = cellTypes.BASE;
     grid[btmLeft.y - 1][btmLeft.x] = cellTypes.BASE;
     grid[btmLeft.y - 1][btmLeft.x + 1] = cellTypes.BASE;
 
     // test pathfinding
-    grid[1][15] = cellTypes.BLOCK;
-    grid[2][15] = cellTypes.BLOCK;
-    grid[3][15] = cellTypes.BLOCK;
-    grid[4][15] = cellTypes.BLOCK;
-    grid[5][15] = cellTypes.BLOCK;
-    grid[6][15] = cellTypes.BLOCK;
+    grid[1][15] = cellTypes.WALL;
+    grid[2][15] = cellTypes.WALL;
+    grid[3][15] = cellTypes.WALL;
+    grid[4][15] = cellTypes.WALL;
+    grid[5][15] = cellTypes.WALL;
+    grid[6][15] = cellTypes.WALL;
 
 })();
 console.log(grid);
@@ -66,7 +66,7 @@ function getGraph() {
         graph[i] = [];
         for (let j = 0; j < grid[i].length; j++) {  // width
             switch (grid[i][j]) {
-                case cellTypes.BLOCK:
+                case cellTypes.WALL:
                 case cellTypes.TOWER:
                     graph[i][j] = 0;  // impassible (infinity wight)
                     break;
--- public/js/tower-defence/game-play.js
+++ public/js/tower-defence/game-play.js
@@ -24,7 +24,7 @@ const gamePlayState = new Phaser.Class({
 
         const truck = scene.add.image(-500, -500, "truck3b");
         truck.angle = 90;
-        scene.e1 = Enemy(truck, 0.003);
+        scene.e1 = Enemy(truck, 0.003, 75);
         console.log(scene.e1);
     },
 
@@ -41,7 +41,7 @@ const gamePlayState = new Phaser.Class({
 function initFromGrid(scene) {
     for (let i = 0; i < grid.length; i++) {  // height
         for (let j = 0; j < grid[i].length; j++) {  // width
-            if (grid[i][j] === cellTypes.BLOCK) {
+            if (grid[i][j] === cellTypes.WALL) {
                 scene.add.image(j * cellSize.width, i * cellSize.height, "brick1").setOrigin(0, 0);
             }
         }
@@ -50,12 +50,23 @@ function initFromGrid(scene) {
 
 
 // Update
-function Enemy(sprite, moveSpeed) {
+
+/**
+ * A closure creating a Enemy.
+ * @param sprite The sprite for this enemy. The enemy will move this sprite.
+ * @param moveSpeed This enemies move speed.
+ * @param healthPoints This enemies starting health points.
+ * @param onDeath A call back that will be called on the death of this Enemy.
+ * @returns {{damage: function(number), move: function, sprite: *}}
+ * @constructor
+ */
+function Enemy(sprite, moveSpeed, healthPoints, onDeath, onBase) {
     let pathPoints;
     let moveIdx = 0;
     let atBase = false;
     const spriteRotation = sprite.rotation;  // should be passed in facing right
-
+    let hp = healthPoints;
+    let alive = true;
 
     function move() {
         if (!pathPoints) {
@@ -69,7 +80,6 @@ function Enemy(sprite, moveSpeed) {
             const newY = gy * cellSize.height + (cellSize.width / 2);
             const angle = Phaser.Math.Angle.Between(sprite.x, sprite.y, newX, newY);
             sprite.rotation = spriteRotation + angle;
-            console.log(angle);
             sprite.x = newX;
             sprite.y = newY;
             moveIdx += moveSpeed;
@@ -91,7 +101,92 @@ function Enemy(sprite, moveSpeed) {
         }
     }
 
-    return {sprite, move}
+    /**
+     * Reduce this enemies health
+     * @param damageAmount The number of hp to deduct
+     * @returns {boolean|undefined} True if damage taken; false if already dead; undefined if damaged and is now dead
+     */
+    function damage(damageAmount) {
+        if (!alive) return false;  // return false if the enemy is already dead
+        hp -= damageAmount;  // damage the enemy
+        if (isDead()) {
+            alive = false;
+            onDeath();
+            return undefined;  // return undefined if the enemy is now dead
+        }
+        return true;  // return true if damage taken
+    }
+
+    function isDead() {
+        return (hp < 0);
+    }
+
+    return {sprite, move, damage}
+}
+
+// Update
+function Tower(sprite, range, damage, fireRate) {
+    let targets = [];  // list of all enemies in range
+    let target;  // the current enemy being targeted
+
+    /**
+     * Goes through all the given enemies and stores them if in range
+     * @param enemies All of the Enemy objects
+     */
+    function resetTargets(enemies) {
+        targets = enemies.filter(isInRange);
+    }
+
+    /**
+     * Determine if the given enemy is within this towers range
+     * @param enemy
+     * @returns {boolean} True when in range, else false
+     */
+    function isInRange(enemy) {
+        return Phaser.Math.Distance.Between(sprite.x, sprite.y, enemy.sprite.x, enemy.sprite.y) < range;
+    }
+
+    /**
+     * Confirm that the current target is in range and non-null
+     * @author: jk
+     * @returns {boolean} True when confirmed, else false.
+     */
+    function confirmTarget() {
+        if (!target && target.alive) {
+            if (!targets) {  // if no current target get the next one in range
+                for (let i = 0; i < targets.length; i++) {
+                    if (isInRange(targets[i]) && targets[i].alive) {
+                        target = targets[i];
+                        targets.splice(0, i);  // remove all enemies before the one found
+                        break;
+                    }
+                }
+            }
+        }
+        return !!target;
+    }
+
+    /**
+     * Inflict damage on the target. Handels cases where the target is already dead or is null by finding a new one.
+     * @returns {boolean}
+     */
+    function shoot() {
+        const target = target;  // store the target in case the property changes
+        if (confirmTarget()) {
+            switch(target.damage(damage)) {
+                case false:
+                    return shoot();  // bad target so try again
+                case true:
+                    // do nothing
+                    return true;
+                case undefined:
+                    targets.splice(targets.indexOf(target), 1);  // remove the enemy from the targets
+                    return true;
+            }
+        }
+        return false; // no targets
+    }
+
 }
 
 
