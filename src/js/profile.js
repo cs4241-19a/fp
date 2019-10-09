cd20cb8b90787345695b5b7594ffc70c8a573c52
manasmehta18 <manasmehta.18@gmail.com>
2019-10-09 04:42:03 -0400

    added profile css, html and js files and added navbar with function btns

---
+++ src/js/profile.js
@@ -0,0 +1,42 @@
+//renders the components for the login page
+
+import React from "react";
+import ReactDOM from "react-dom";
+import profileApp from "./profileApp";
+
+ReactDOM.render(<profileApp />, document.getElementById("root"));
+
+window.onload = function () {
+    fetch( '/receive')
+        .then( function( response ) {
+            return response.json();
+        }).then(function (response) {
+        window.localStorage;
+        let user = localStorage.getItem('currUser');
+        console.log(user)
+        for(let i = 0; i < Object.keys(response).length; i++) {
+            if(response[i].username === user) {
+                document.getElementById("nameCurr").innerText = JSON.stringify(response[i].firstName).replace(/^"(.*)"$/, '$1');
+                break;
+            }
+        }
+    })
+    document.getElementById("homeGo").onclick = home
+    document.getElementById("logoutBtn").onclick = logout
+    document.getElementById("profileGo").onclick = myProfile
+}
+
+function home(e) {
+    e.preventDefault();
+    window.location = "/main.html"
+}
+
+function myProfile(e) {
+    e.preventDefault();
+    window.location = "/profile.html"
+}
+
+function logout(e) {
+    e.preventDefault();
+    window.location = "/login.html"
+}
