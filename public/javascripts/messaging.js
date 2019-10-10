120e78667703cb82065c638246c59eb49ce6d20e
Glitch (mersair-fp) <none>
2019-10-10 02:01:06 +0000

    🐃🚡 Checkpoint ./public/stylesheets/style.css:6267243/1 ./public/javascripts/messaging.js:6267243/5366

--- public/javascripts/messaging.js
+++ public/javascripts/messaging.js
@@ -1,23 +1,44 @@
-window.onload = function(){
+window.onload = function() {
   fetch("api/users/getUsers", {
     headers: {
       "Content-Type": "application/json"
     },
-    method: "POST",
+    method: "POST"
   }).then(function(res) {
     res.json().then(function(ret) {
-     for(let i = 0; i < ret.length; i++){
-       let l = document.createElement("li");
-       let a = document.createElement("a");
-       a.href = "#"
-       a.class = "pure-menu-link"
-       l.class = "pure-menu-item";
-       a.innerHTML = ret[i].username;
-       l.style = "padding: auto; margin:10px; color:white; font-weight:bold;"
-       a.style = "color:white; text-decoration: none"
-       l.appendChild(a);
-       document.getElementById("people").appendChild(l)
-     }
+      for (let i = 0; i < ret.length; i++) {
+        let l = document.createElement("li");
+        let a = document.createElement("a");
+        a.href = "#";
+        a.class = "pure-menu-link";
+        l.class = "pure-menu-item";
+        a.innerHTML = ret[i].username;
+        l.style = "padding: auto; margin:10px; color:white; font-weight:bold;";
+        l.id = ret[i].username;
+        a.onclick = function() {
+          document.getElementById("t").innerHTML =
+            "Messages with " + ret[i].username;
+          // SET MESSAGES
+        };
+        a.style = "color:white; text-decoration: none";
+        l.appendChild(a);
+        document.getElementById("people").appendChild(l);
+      }
     });
   });
+};
+
+function updateChat(){
+   fetch("messaging/users/getUsers", {
+    headers: {
+      "Content-Type": "application/json"
+    },
+    method: "POST"
+  }).then(function(res) {
+    res.json().then(function(ret) {
+      for (let i = 0; i < ret.length; i++) {
+        
+      }
+    });
+  }); 
 }
