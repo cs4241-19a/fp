f52405ef1c46ecf7b6e69b9bb31953cca87a6412
Percy Jiang <hjiang2@wpi.edu>
2020-01-21 18:20:11 -0800

    'hi'

--- public/main.js
+++ public/main.js
@@ -1,16 +1,10 @@
-const login = document.getElementById('login')
-const username = document.querySelector('#username')
-const password = document.querySelector('#password')
-const hint = document.getElementById('hint')
-
-const makeBody = function() {
-    const json = { username: username.value, password: password.value };
-    return JSON.stringify(json);
-};
-login.onclick = function(e) {
+document.getElementById('loginBtn').onclick = function(e) {
     fetch('/login', {
             method: 'POST',
-            body: makeBody(),
+            body: JSON.stringify({
+                username: document.querySelector('#username').value,
+                password: document.querySelector('#password').value
+            }),
             headers: { 'Content-Type': 'application/json' }
         })
         .then(res => res.json())
@@ -20,14 +14,13 @@ login.onclick = function(e) {
         })
         .catch(err => {
             console.log(err)
-            hint.innerHTML = 'Invalid Input'
+            document.getElementById('hint').innerHTML = 'Invalid Input'
         })
     e.preventDefault();
     return false;
 };
 
-const register = document.getElementById('register');
-register.onclick = function(e) {
+document.getElementById('registerBtn').onclick = function(e) {
     location.href = 'register/register.html';
     e.preventDefault();
     return false;
